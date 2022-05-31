3c991169dbdf5edf4f65dfb97b035a91f3c5e5b7
Luohao Xu <1976074396@qq.com>
2022-05-31 16:43:12 +0800

    compare with sklearn

--- src/linear_model/LinearRegression.py
+++ src/linear_model/LinearRegression.py
@@ -5,30 +5,40 @@ Created on Wed May 18 23:40:04 2022
 @author: Luohao Xu
 """
 import numpy as np
-
+import sklearn.linear_model as sk
+from sklearn.datasets import fetch_california_housing
 # 线性回归
 class LinearRegression:
     
-    def __init__(self, X, y):
-        self.m = len(y) # 样本总数
-        self.d = len(X[0]) # 特征总数
-        self.X = np.c_[np.matrix(X),np.ones(self.m)]
-        self.y = np.matrix(y).T
-        self.fit()
+    def __init__(self):
+        pass
     
-    def fit(self):
-        X, y = self.X, self.y
+    def fit(self,X,y):
+        m = len(y) # 样本总数
+        X = np.c_[np.matrix(X),np.ones(m)]
+        y = np.matrix(y).T
         self.w = np.linalg.pinv(X.T*X)*X.T*y
         
-    def predict(self, predict_X):
-        X = np.c_[np.matrix(predict_X), np.ones(len(predict_X))]
+    def predict(self, X):
+        X = np.c_[np.matrix(X), np.ones(len(X))]
         predict_y = X * self.w
-        return predict_y
+        return np.array(predict_y.T.tolist()[0])
         
 if __name__ == "__main__":
-    X = [[1],[2],[3],[4]]
-    y = [5,9,13,17]
-    lr = LinearRegression(X, y)
-    result = lr.predict([[5],[6]])
-    print(result)
+    data = fetch_california_housing()
+    X = data.data
+    y = data.target
+
+    my_lr = LinearRegression()
+    my_lr.fit(X, y)
+    res0 = my_lr.predict(X)
+    
+    sk_lr = sk.LinearRegression()
+    sk_lr.fit(X, y)
+    res1 = sk_lr.predict(X)
+    
+    # 比较sklearn和自己实现的结果
+    print(res0)
+    print(res1)
+
     
